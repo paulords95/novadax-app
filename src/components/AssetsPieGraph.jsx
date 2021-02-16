f95591387489c9920d8920fe1f2ff8bf310559a2
paulords95 <paulords95@gmail.com>
2021-02-16 13:59:23 -0300

    added real data to graph

--- src/components/AssetsPieGraph.jsx
+++ src/components/AssetsPieGraph.jsx
@@ -1,64 +1,101 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, View } from "react-native";
+import { Dimensions, View, Text } from "react-native";
 
 import { PieChart } from "react-native-chart-kit";
 
 import { apiLocal } from "../services/api";
 
 const AssetsPieGraph = () => {
-  const data = [
+  const [totalNetWorth, setTotalNetWorth] = useState(0);
+  const [data, setData] = useState([
     {
-      name: "Seoul",
-      population: 215,
-      color: "rgba(131, 167, 234, 1)",
-      legendFontColor: "#7F7F7F",
+      name: "Carregando",
+      balance: 0,
+      color: "#C6BDBD",
+      legendFontColor: "#C6BDBD",
       legendFontSize: 15,
     },
+  ]);
+  const [load, setLoad] = useState(false);
+  let [, setState] = useState();
 
-    {
-      name: "New York",
-      population: 85,
-      color: "#ffffff",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15,
-    },
-    {
-      name: "Moscow",
-      population: 119,
-      color: "rgb(0, 0, 255)",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15,
-    },
-  ];
+  const graphColor = (name) => {
+    let color = "";
+    if (name == "BTC") {
+      color = "#A658CC";
+    }
+    if (name == "DOGE") {
+      color = "#6B029D";
+    }
+    if (name == "ETH") {
+      color = "#BC7FDA";
+    }
+    return color;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const dataApi = await apiLocal.get("/walletbalancepercentage");
+      const graphData = [];
+      for (let i of dataApi.data) {
+        setTotalNetWorth(i.totalAssets);
+        if (i.balance != undefined) {
+          graphData.push({
+            name: i.currency,
+            balance: parseFloat(i.percentage),
+            color: graphColor(i.currency),
+            legendFontColor: "white",
+            legendFontSize: 15,
+          });
+        }
+      }
+      setLoad(true);
+      setData(graphData);
+    })();
+  }, []);
+
+  const RenderPieChart = (props) => {
+    if (load) {
+      return (
+        <PieChart
+          data={props.populate}
+          width={Dimensions.get("window").width - 30}
+          height={230}
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+              borderWidth: 1,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+          accessor={"balance"}
+          backgroundColor={"transparent"}
+          paddingLeft={"20"}
+          center={[10, 0]}
+          style={{
+            marginTop: 70,
+          }}
+        />
+      );
+    } else {
+      return <Text>Carregadno</Text>;
+    }
+  };
 
   return (
     <View style={{}}>
-      <PieChart
-        data={data}
-        width={Dimensions.get("window").width}
-        height={270}
-        chartConfig={{
-          backgroundColor: "#e26a00",
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#ffa726",
-          },
-        }}
-        accessor={"population"}
-        backgroundColor={"transparent"}
-        paddingLeft={"0"}
-        center={[20, 20]}
-        absolute
-      />
+      <RenderPieChart populate={data} />
+      <Text>{totalNetWorth}</Text>
     </View>
   );
 };
