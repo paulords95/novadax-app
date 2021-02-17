c6215af44848f4b18ce4f03a32b0d4c357d1d149
paulords95 <paulords95@gmail.com>
2021-02-16 23:44:43 -0300

    changed api address

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const apiDax = axios.create({
 });
 
 const apiLocal = axios.create({
-  baseURL: "http://192.168.0.11:1234",
+  baseURL: "http://191.252.205.198:9433",
 });
 
 module.exports = { apiDax, apiLocal };
